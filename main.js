c4b0371446c2104881a40eb2328c72171daf476b
meowcodes <meowcodes@gmail.com>
2018-12-07 14:11:33 -0800

    add functionality

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 var card = document.getElementsByClassName("card");
 
+var count = 0;
+var imgSrc = [];
+
 for(var i=0; i<card.length; i++){
   card[i].addEventListener("click", function() {
-    console.log(this);
-    this.firstElementChild.style.display = "block";
+    if(count < 2){
+      console.log(this);
+      this.firstElementChild.style.display = "block";
+      imgSrc.push(this);
+      count += 1;
+      if(count === 2){
+        setTimeout(function() {
+          if(imgSrc[0].firstElementChild.src === imgSrc[1].firstElementChild.src){
+            imgSrc[0].firstElementChild.classList.remove("card-unmatched");
+            imgSrc[0].firstElementChild.classList.add("card-matched");
+            imgSrc[1].firstElementChild.classList.remove("card-unmatched");
+            imgSrc[1].firstElementChild.classList.add("card-matched");
+          }else {
+            imgSrc[0].firstElementChild.style.display = "none";
+            imgSrc[1].firstElementChild.style.display = "none";
+          }
+          count = 0;
+          imgSrc = [];
+        },1000);
+        
+      }
+    }else {
+      console.log("no more!");
+    }
   });
+
+
+  // if(count === 2){
+  //   if(imgSrc[0] === imgSrc[1]){
+
+  //   }
+  // }
 }
